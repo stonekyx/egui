7c219bf4d95c41aa40cf1082b2b87afaa2336c2d
stonekyx <stonekyx@gmail.com>
2013-09-06 17:28:19 +0800

    add resource files into distribution
    Cursor icon files in resource/ will be installed into $pkgdatadir, and
    window_manager/cursor.c now use a path given by automake to find default
    cursor file.

---
+++ resource/local.mk
@@ -0,0 +1,11 @@
+nobase_pkgdata_DATA += %D%/aero_arrow_xl.cur \
+					   %D%/bluearrow.cur \
+					   %D%/busy_rl.cur \
+					   %D%/crosshair.cur \
+					   %D%/cursor1.cur \
+					   %D%/default.cur \
+					   %D%/greenarrow.cur \
+					   %D%/move_rl.cur \
+					   %D%/size4_rm.cur \
+					   %D%/up_il.cur \
+					   %D%/wait_m.cur
