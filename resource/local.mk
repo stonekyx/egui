35f0cfbb69952cd8698f7d3c5fa353527a08ba87
Stone Kang <stonekyx@gmail.com>
2014-03-16 14:15:48 +0800

    fix resource for distribution

--- resource/local.mk
+++ resource/local.mk
@@ -1,11 +1,13 @@
-nobase_pkgdata_DATA += %D%/aero_arrow_xl.cur \
-					   %D%/bluearrow.cur \
-					   %D%/busy_rl.cur \
-					   %D%/crosshair.cur \
-					   %D%/cursor1.cur \
-					   %D%/default.cur \
-					   %D%/greenarrow.cur \
-					   %D%/move_rl.cur \
-					   %D%/size4_rm.cur \
-					   %D%/up_il.cur \
-					   %D%/wait_m.cur
+resourcedir = $(pkgdatadir)/resource
+
+dist_resource_DATA = %D%/aero_arrow_xl.cur \
+					 %D%/bluearrow.cur \
+					 %D%/busy_rl.cur \
+					 %D%/crosshair.cur \
+					 %D%/cursor1.cur \
+					 %D%/default.cur \
+					 %D%/greenarrow.cur \
+					 %D%/move_rl.cur \
+					 %D%/size4_rm.cur \
+					 %D%/up_il.cur \
+					 %D%/wait_m.cur
