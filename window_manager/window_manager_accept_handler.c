0c785389ef31d4073747dafa7368217abc4481dd
FangYuanClaire <598311998@qq.com>
2014-04-07 14:31:58 +0800

    creat a welcome interface,close it when the first application comes.

--- window_manager/window_manager_accept_handler.c
+++ window_manager/window_manager_accept_handler.c
@@ -41,6 +41,7 @@ si_t window_manager_accept_handler(struct egui_uds* uds_ptr, addr_t arg)
 {
     struct application_info app_info;
     struct application_info* app_info_ptr = NULL;
+    static int flag = 0;
     NOT_USED(arg);
 
 	application_info_init(&app_info);
@@ -50,7 +51,12 @@ si_t window_manager_accept_handler(struct egui_uds* uds_ptr, addr_t arg)
 		EGUI_PRINT_ERROR("failed to accept client uds");
 		return SELECTER_RETURN_TYPE_END;
 	}
-	
+	if(0 == flag)
+    {
+        screen_clear_black(&global_screen);
+        screen_flush(0,0,global_screen.width,global_screen.height);
+        flag = 1;
+    }
 	/* 将用户应用程序加入到向量 */
 	vector_push_back(&global_wm.application_info_vector, &app_info, sizeof(struct application_info));
 	app_info_ptr = vector_back(&global_wm.application_info_vector);
